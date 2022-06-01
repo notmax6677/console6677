c788437e14da2c2e584f4b8c60c48f4c37f22d3d
notmax6677 <70626784+notmax6677@users.noreply.github.com>
2022-06-01 21:31:30 +0400

    Add files via upload

---
+++ src/js/Messages.js
@@ -0,0 +1,37 @@
+export default {
+    // welcome message (might look a bit distorted in code but looks well when processed and rendered)
+    welcome: " \n\
+                           _           ____     ____    ______  ______\n\
+                          | |         / ___|   / ___|  |___  / |___  /\n\
+  ___ ___  _ __  ___  ___ | | ___    / /___   / /___      / /     / /\n\
+ / __/ _ \\| '_ \\/ __|/ _ \\| |/ _ \\   | ___ \\  | ___ \\    / /     / /\n\
+| (_| (_) | | | \\__ \\ (_) | |  __/   | \\_/ |  | \\_/ |  ./ /    ./ /\n\
+ \\___\\___/|_| |_|___/\\___/|_|\\___|   \\_____/  \\_____/  \\_/     \\_/\n\
+\n\
+Welcome to console6677!\n\
+\n\
+Get started by typing `help`!",
+
+    // help message
+    help: "Available commands: help, about, clear, github, date, echo, contact, theme, reboot",
+
+    // theme help message
+    themeHelp: "Theme command, e.g: \ntheme [name] \n\nAvailable themes: [default, gruvbox, gruvbox-light, solarized, solarized-light, dracula]",
+
+    // theme error message
+    themeError: "Theme not found, for more info, type `theme help`.",
+
+    // about message
+    about: "Hello! My name is Max, as of the creation of this project i'm 14 and i'm from Ukraine & Canada.\n\n\
+I enjoy programming, food, working out, and watching Netflix!\n\n\
+I made this because i saw similar projects online and decided to see if i could make something of this sort. \n\n\
+I've just started learning a new web framework called SolidJS and wanted to put my skills to the test.\n\n\
+Anyway, i hope you enjoy this little project of mine!\n\n\
+type `contact` for a bit of my contact info.",
+
+    // contact message
+    contact: "Github: notmax6677\nEmail: max.meln1210@gmail.com",
+
+    // name
+    name: "guest@console6677 $",
+}
