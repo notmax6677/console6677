748e5e8b3b44c2eb0f9df60f3455b7c48884ceb0
notmax6677 <70626784+notmax6677@users.noreply.github.com>
2022-06-01 22:03:06 +0400

    Delete source directory

--- source/src/index.jsx
+++
@@ -1,9 +0,0 @@
-/* @refresh reload */
-import { render } from 'solid-js/web';
-
-import App from './App';
-
-// css
-import "./css/index.css"
-
-render(() => <App />, document.getElementById('root'));
